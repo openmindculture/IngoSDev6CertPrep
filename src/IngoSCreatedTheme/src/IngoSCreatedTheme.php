7fdc9a09e4495242746d6369a20ff93eaf399eac
Ingo Steinke <ingosteinke@gmx.de>
2024-01-04 19:32:18 +0100

    add example theme structure

---
+++ src/IngoSCreatedTheme/src/IngoSCreatedTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace IngoSCreatedTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class IngoSCreatedTheme extends Plugin implements ThemeInterface
+{
+}
