68426e68b3eadf8ced5ecb05adf1cc1e2fd35070
Ingo Steinke <ingosteinke@gmx.de>
2024-01-18 20:44:27 +0100

    cleanup and theme dev WIP

--- src/IngoSDemoTogetherTheme/src/Resources/app/storefront/src/main.js
+++ src/IngoSDemoTogetherTheme/src/Resources/app/storefront/src/main.js
@@ -1,15 +1,5 @@
-console.log('main.js in theme plugin');
-const ingosthetestconstant = "implicit string assigned using const to verify if Shopware has a default transpilation";
-console.log(ingosthetestconstant);
-// It has and it even optimizes away the above variable.
-// BUT where is my function below? no matter, as we don't need to expose it globally
-// Next question: do we need to wait for document complete explicitly?
-// AND if the stuff gets transpiled anyway, can we and should we use TypeScript here?
-console.log('after REALLY renaming main.js to main.ts');
-
-// <plugin root>/src/Resources/app/storefront/src/main.js
-// Import all necessary Storefront plugins
 import IngoSDemoTogetherTheme from './ingo-s-demo-together-theme/ingo-s-demo-together-theme';
+// Import other plugins if necessary
 // Register your plugin via the existing PluginManager
 const PluginManager = window.PluginManager;
 PluginManager.register('IngoSDemoTogetherTheme', IngoSDemoTogetherTheme);
