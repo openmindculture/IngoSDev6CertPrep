3207273de24771e77c8ea48a473b7e3317afea08
Ingo Steinke <ingosteinke@gmx.de>
2024-01-24 16:55:09 +0100

    add custom fields (WIP)

--- src/IngoSDemoTogetherTheme/src/Core/Content/Faq/FaqDefinition.php
+++ src/IngoSDemoTogetherTheme/src/Core/Content/Faq/FaqDefinition.php
@@ -15,6 +15,38 @@ use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
 class FaqDefinition extends EntityDefinition
 {
     public const ENTITY_NAME = 'faq';
+    protected $technicalName;
+    protected $question;
+    protected $answer;
+
+    // TODO does the order of variables, fields, functions matter?
+    // as Database fields, possible parameters?
+
+    public function getTechnicalName(): string
+    {
+        return $this->technicalName;
+    }
+
+    public function getQuestion(): string
+    {
+        return $this->question;
+    }
+
+    public function getAnswer(): string
+    {
+        return $this->answer;
+    }
+
+    // TODO why is getQuestion marked as unused, but not getTechnicalName?
+    // TODO why is $answer makred as unused but not $question?
+    // or is it a bug and it only marks the latest occurence?
+
+    // TODO add setters for Q,A
+    public function setTechnicalName(string $technicalName)
+    {
+        $this->technicalName = $technicalName;
+    }
+
     public function getEntityName(): string
     {
         return self::ENTITY_NAME;
@@ -37,6 +69,7 @@ class FaqDefinition extends EntityDefinition
             new StringField('name', 'name'),
             new CreatedAtField(),
             new UpdatedAtField(),
+            // TODO add $technicalName, $question, $answer here?
         ]);
     }
 }
