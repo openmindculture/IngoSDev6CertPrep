fa0a5c3665c6c973f0474149b3e874e716d3514f
Ingo Steinke <ingosteinke@gmx.de>
2024-01-25 18:44:54 +0100

    add database migration for faq entity

---
+++ src/IngoSDemoTogetherTheme/src/Migration/Migration1706197000Faq.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace IngoS\DemoTogetherTheme\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1706197000Faq extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1706197000;
+    }
+
+    public function update(Connection $connection): void
+    {
+        // implement update
+        $sql=<<<SQL
+    CREATE TABLE `faq` (
+        `id` BINARY(16) NOT NULL,
+        `name` VARCHAR(255) NULL,
+        `created_at` DATETIME(3) NOT NULL,
+        `updated_at` DATETIME(3) NULL,
+        PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+SQL;
+        $connection->executeUpdate($sql);
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}
---
+++ src/IngoSDemoTogetherTheme/src/Migration/faq.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `faq` (
+    `id` BINARY(16) NOT NULL,
+    `name` VARCHAR(255) NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3) NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
